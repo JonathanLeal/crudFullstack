4c15daeebb8407abbda415899e813673a2c02cba
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-05-22 10:47:42 -0600

    llenando tabla back y front

--- public/js/Persona.js
+++ public/js/Persona.js
@@ -1,1 +1,36 @@
-alert('funcionando');
+var personas = [];
+function obtenerPersonas() {
+    axios({
+        method:'GET',
+        url: 'http://127.0.0.1:8000/personas/list',
+        responseType: 'json'
+    }).then(response => {
+        console.log(response);
+        this.personas = response.data.datos
+        llenarTabla();
+    }).catch(err => {
+        console.log(err);
+    });
+}
+obtenerPersonas();
+
+function llenarTabla() {
+    for (let i = 0; i < personas.length; i++) {
+        document.querySelector('#tablaPersonas tbody').innerHTML += 
+        `
+            <tr>
+                <td>${personas[i].id}</td>
+                <td>${personas[i].Nombre}</td>
+                <td>${personas[i].edad}</td>
+                <td>
+                    <button type="button" onclick='editar'(${i})>Editar</button>
+                    <button type="button" onclick='eliminar'(${i})>Eliminar</button>
+                </td>
+            </tr>
+        `;
+    }
+}
+
+function eliminar(indice) {
+    console.log('eliminando con el indice: '+index)
+}
--- resources/views/Personas.blade.php
+++ resources/views/Personas.blade.php
@@ -4,34 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{ asset('js/Persona.js') }}"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Edad</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>id</td>
-                <td>nombre</td>
-                <td>edad</td>
-                <td>
-                    <button type="button">Editar</button>
-                    <button type="button">Eliminar</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <div>
+            <div>
+                <table id="tablaPersonas" border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Edad</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{ asset('js/Persona.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
